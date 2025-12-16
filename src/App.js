44a62fc6de6affc9cef9685808e978e635a0db77
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-16 08:26:49 -0500

    Implement derived state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import { useState } from "react";
 export default function App() {
   // Lifting state, moving state to closest common parent
   const [items, setItems] = useState([]);
+
   // lift function so it lives where state does
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
+  function handleRemoveItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handlePackItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
-      <Stats />
+      <PackingList
+        items={items}
+        onRemoveItem={handleRemoveItem}
+        onPackItem={handlePackItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -59,12 +75,17 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onRemoveItem, onPackItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item
+            item={item}
+            key={item.id}
+            onRemoveItem={onRemoveItem}
+            onPackItem={onPackItem}
+          />
         ))}
       </ul>
     </div>
@@ -72,21 +93,40 @@ function PackingList({ items }) {
 }
 
 // function Item({ id, description, quantity, packed }) {
-function Item({ item }) {
+function Item({ item, onRemoveItem, onPackItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onClick={() => onPackItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>{" "}
-      <button>❌</button>
+      <button onClick={() => onRemoveItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%) 🧳</em>
+      <em>
+        {percentage === 100
+          ? `You've packed everything! You're ready to go!`
+          : `You have ${numItems} items on your list, and you already packed ${packedItems} (${percentage}%) 🧳`}
+      </em>
     </footer>
   );
 }
